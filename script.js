f20ab7516b7192fbd85be52dba1c28b23225f495
SATHIYAPRABHA120 <168350573+SATHIYAPRABHA120@users.noreply.github.com>
2024-07-09 18:52:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const typedTextSpan = document.querySelector(".typed-text")
+const cursor = document.querySelector(".cursor")
+
+let words = ['Awesome','Fun','Genuine','Optimistic','Weird','Cool','Unique','Thoughtful','Kind-hearted','Unstoppable','Inspiring','Exceptionally Hardworking','Talented','Brave','Generous']
+const typingDelay = 50
+const erasingDelay = 50
+const newLetterDelay = 1000
+let index = 0
+let charIndex = 0
+
+document.addEventListener('DOMContentLoaded',()=>{
+    if(words.length){
+        setTimeout(type,newLetterDelay)
+    }
+})
+
+function type(){
+    if(charIndex<words[index].length){
+        typedTextSpan.textContent += words[index].charAt(charIndex)
+        charIndex++
+        setTimeout(type,typingDelay)
+    }
+    else{
+        setTimeout(erase,newLetterDelay)
+    }
+}
+
+function erase(){
+    if(charIndex > 0){
+        typedTextSpan.textContent = words[index].substring(0, charIndex - 1)
+        charIndex--
+        setTimeout(erase,erasingDelay)
+    }
+    else{
+        index++
+        if(index >= words.length){
+            index = 0
+        }
+        setTimeout(type,typingDelay + 1100)
+    }
+}
